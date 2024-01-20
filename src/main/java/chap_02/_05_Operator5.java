da259eeef07f95b653758d97fc8ee95b428fa67e
DemianStudio <rudejrqkr97@gmail.com>
2024-01-20 22:46:22 +0900

    :zap: 12. Operator5
    + _05_Operator5.java

---
+++ src/main/java/chap_02/_05_Operator5.java
@@ -0,0 +1,22 @@
+package chap_02;
+
+public class _05_Operator5 {
+    public static void main(String[] args) {
+        // 삼항 연산자
+//        결과 = (조건) ? (참의 경우 결과값) : (거짓의 경우 결과값)
+        int x = 5;
+        int y = 3;
+        int max = (x > y) ? x : y;
+        System.out.println("max = " + max);
+
+        int min = (x < y) ? x : y;
+        System.out.println("min = " + min);
+
+        boolean b = (x == y) ? true : false;
+        System.out.println("b = " + b);
+
+        String s = (x != y) ? "달라요" : "같아요";
+        System.out.println("s = " + s);
+
+    }
+}
