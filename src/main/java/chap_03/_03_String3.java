258bff37b787de410674bf5824a6201097dca440
DemianStudio <rudejrqkr97@gmail.com>
2024-01-21 00:15:45 +0900

    :fire: 15. String3
    + _03_String3.java

---
+++ src/main/java/chap_03/_03_String3.java
@@ -0,0 +1,33 @@
+package chap_03;
+
+public class _03_String3 {
+    public static void main(String[] args) {
+        // 문자열 비교
+        String s1 = "Java";
+        String s2 = "Python";
+
+        System.out.println(s1.equals("Java"));  // 문자열 내용이 같으면 true, 다르면 false
+        System.out.println(s2.equalsIgnoreCase("python"));  // 대소문자 구분 없이 문자열 내용이 같은지 여부 체크
+
+        System.out.println();
+
+        // 문자열 비교 심화
+        s1 = "1234";    // 벽에 붙은 메모지의 비밀번호 정보1(참조)
+        s2 = "1234";    // 벽에 붙은 메모지의 비밀번호 정보2(참조)
+        System.out.println(s1.equals(s2));  // true(내용을 비교)
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(System.identityHashCode(s2));
+        System.out.println(s1 == s2);   // true(참조를 비교)
+
+        System.out.println();
+
+        s1 = new String("1234");
+        s2 = new String("1234");
+        System.out.println(s1.equals(s2));  // true
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(System.identityHashCode(s2));
+        System.out.println(s1 == s2);   // false
+
+
+    }
+}
