94b7392527a775219c8109601e9317d9e5312fd6
DemianStudio <rudejrqkr97@gmail.com>
2024-01-22 23:58:31 +0900

    :bug: 26. break
    + _10_Break.java

---
+++ src/main/java/chap_04/_10_Break.java
@@ -0,0 +1,36 @@
+package chap_04;
+
+import java.sql.SQLOutput;
+
+public class _10_Break {
+    public static void main(String[] args) {
+        // Break
+        // 치킨집에서 매일 20마리만 판매(1인당 1마리만 구매 가능)
+        // 손님이 50명 대기
+
+        // for문
+        int max = 20;
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + "번 손님, 주문하신 치킨 나왔습니다.");
+            if (i == max) {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;  // 반복문 탈출
+            }
+        }
+        System.out.println("영업을 종료합니다.");
+
+        System.out.println("==========");
+
+        // While
+        int index = 1;  // 손님 대기 번호
+        while (index <= 50) {
+            System.out.println(index + "번 손님, 주문하신 치킨 나왔습니다.");
+            if (index == max) {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;
+            }
+            index++;
+        }
+        System.out.println("영업을 종료합니다.");
+    }
+}
