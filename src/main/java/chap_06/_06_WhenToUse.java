7d3a5795ccc8d146b097b5bc460d9256ffc7226a
DemianStudio <rudejrqkr97@gmail.com>
2024-01-29 15:28:12 +0900

    :sparkles: 38. WhenToUse
    + _06_WhenToUse.java

---
+++ src/main/java/chap_06/_06_WhenToUse.java
@@ -0,0 +1,48 @@
+package chap_06;
+
+import javax.swing.*;
+
+public class _06_WhenToUse {
+    public static int getPower(int number) {
+        return getPower(number,2);
+    }
+
+    public static int getPower(int number, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= number;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        // 메소드가 필요한 이유
+
+        // 2의 2승을 구하기
+        int result = 1;
+        for (int i = 0; i < 2; i++) {
+            result *= 2;
+        }
+        System.out.println(result);
+
+        // 3의 3승을 구하기
+        result = 1;
+        for (int i = 0; i < 3; i++) {
+            result *= 3;
+        }
+        System.out.println(result);
+
+        // 4의 2승을 구하기
+        result = 1;
+        for (int i = 0; i < 2; i++) {
+            result *= 4;
+        }
+        System.out.println(result);
+
+        System.out.println();
+
+        System.out.println(getPower(2, 2));
+        System.out.println(getPower(3, 3));
+        System.out.println(getPower(4, 2));
+
+    }
+}
