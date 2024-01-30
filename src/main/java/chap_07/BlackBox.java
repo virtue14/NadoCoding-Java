51dab92046ebfe6829bcd7ade6bdb244385ce593
DemianStudio <rudejrqkr97@gmail.com>
2024-01-30 18:12:26 +0900

    :memo: 44. Method
    + _04_Method.java
    [update] BlackBox.java

--- src/main/java/chap_07/BlackBox.java
+++ src/main/java/chap_07/BlackBox.java
@@ -7,4 +7,26 @@ public class BlackBox {
     int price;
     String color;
     static boolean canAutoReport = false;   // 자동 신고 기능
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) {
+            return 9;
+        } else if (type == 2) {
+            return 1;
+        }
+        return 10;
+    }
 }
