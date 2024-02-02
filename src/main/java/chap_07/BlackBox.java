e8d9fe4a8571369392d33a0b7fc6873910400996
DemianStudio <rudejrqkr97@gmail.com>
2024-02-02 17:29:59 +0900

    :memo: 45. MethodOverloading
    + _05_MethodOverloading.java
    [update] BlackBox.java

--- src/main/java/chap_07/BlackBox.java
+++ src/main/java/chap_07/BlackBox.java
@@ -1,5 +1,7 @@
 package chap_07;
 
+import java.sql.SQLOutput;
+
 public class BlackBox {
 
     String modelName;
@@ -29,4 +31,23 @@ public class BlackBox {
         }
         return 10;
     }
+
+    // showDataTime: 날짜정보 표시여부
+    // ShowSpeed: 속도정보 표시여부
+    // min: 영상 기록 단위(분)
+    void record(boolean showDataTime, boolean showSpeed, int min) {
+        System.out.println("녹화를 시작합니다.");
+        if (showDataTime) {
+            System.out.println("영상에 날짜정보가 표시됩니다");
+        }
+        if (showSpeed) {
+            System.out.println("영상에 속도정보가 표시됩니다.");
+        }
+        System.out.println("영상은" + min + "분 단위로 기록됩니다.");
+
+    }
+    void recode() {
+        record(true, true, 5);
+    }
+
 }
